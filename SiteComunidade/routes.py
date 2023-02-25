255ff727b504df789e18524f0b866d674440f24f
Danielle Magrini <102551190+daniellemagrini@users.noreply.github.com>
2023-02-25 15:40:22 -0300

    Update routes.py

--- SiteComunidade/routes.py
+++ SiteComunidade/routes.py
@@ -41,7 +41,7 @@ def login():
         else:
             flash('E-mail e/ou senha incorretos!', 'alert-danger')
     if form_criar_conta.validate_on_submit() and 'bt_criar_conta' in request.form:
-        senha_criptografada = bcrypt.generate_password_hash(form_criar_conta.senha.data)
+        senha_criptografada = bcrypt.generate_password_hash(form_criar_conta.senha.data).decode("utf-8")
         usuario = Usuario(username=form_criar_conta.username.data, email=form_criar_conta.email.data, senha=senha_criptografada)
         database.session.add(usuario)
         database.session.commit()
