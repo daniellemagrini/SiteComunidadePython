243eb6cd46d1f5ea89a46ec70b98f206161e36c0
Danielle Magrini <102551190+daniellemagrini@users.noreply.github.com>
2023-02-25 14:19:40 -0300

    Update forms.py

--- SiteComunidade/forms.py
+++ SiteComunidade/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileAllwed
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from SiteComunidade.models import Usuario
